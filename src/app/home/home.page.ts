ba0f91d43660419ec6ad5d531e813d5eda25e14d
GuimasDev <gabrielspdev@gmail.com>
2023-03-14 20:35:30 -0300

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  valorAplicacao!: number;
+  taxaJuros!: number;
+  meses!: number;
+  tipoJuros!: "composto" | "simples";
+  rendimentoTotal!: number;
 
-  constructor() {}
+  constructor() {
+  }
+
+  bt_limpar(): void {
+    this.valorAplicacao = 0;
+    this.taxaJuros = 0;
+    this.meses = 0;
+    this.rendimentoTotal = 0;
+  }
+
+  bt_calcular(): void {
+    //const calcularJurosSimples = (capital: number, taxa: number, meses: number) => capital * taxa * meses / 100;
+    
+    if (this.tipoJuros === "composto" || this.tipoJuros === "simples") {
+      alert(this.tipoJuros);
+      
+    }
+    let juros = this.calcularJurosComposto(this.valorAplicacao, this.taxaJuros, this.meses);
+    this.rendimentoTotal = this.valorAplicacao + juros;
+  }
+
+  calcularJurosComposto(capital: number, taxa: number, meses: number): number {
+    let juros: number;
+    juros = (capital * ((1+(taxa/100)) ** meses)) - capital;
+    return Number(juros.toFixed(2));
+  };
 
 }
